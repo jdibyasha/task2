9c5192ef1279d7a18369e2937dabba61836bf865
jdibyasha <39983067+jdibyasha@users.noreply.github.com>
2023-05-01 14:30:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Child1 from './Child1';
+import Child2 from './Child1';
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2><Child1/></h2>
+      <h2><Child2/></h2>
+    </div>
+  );
+}
